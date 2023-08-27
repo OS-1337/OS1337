bbd8da502833bed71bc395457fb2632c2f28952e
kkarhan <kevin@karhan.guru>
2023-08-28 01:54:51 +0200

    - adding init scripts - removing download-toybox.bins.sh since the provided binaries are too big. - renamed config.linux-kernel.i486.sh to 3.config.linux-kernel.i486.sh

--- scripts/0.start.sh
+++ scripts/0.start.sh
@@ -4,5 +4,13 @@ echo 'Setting up the build envoirment...'
 echo 'creating folders...'
 cd ./..
 mkdir ./build
+mkdir ./build/1440k-fdd
+kmdir ./build/1440k-fdd/rootfs
+kmdir ./build/1440k-fdd/rootfs/dev
+kmdir ./build/1440k-fdd/rootfs/etc
+kmdir ./build/1440k-fdd/rootfs/etc/init.d
+kmdir ./build/1440k-fdd/rootfs/proc
+kmdir ./build/1440k-fdd/rootfs/sys
+kmdir ./build/1440k-fdd/rootfs/tmp
 cd ./scripts
 exit
