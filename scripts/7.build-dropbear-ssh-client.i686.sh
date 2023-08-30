9f57fa1d448861f2eeab142f971a33248e687bcf
kkarhan <kevin@karhan.guru>
2023-08-30 02:28:39 +0200

    added Makefile for dbclient as per dropbearmulti : dropbear-2022.83.Makefile

--- scripts/7.build-dropbear-ssh-client.i686.sh
+++ scripts/7.build-dropbear-ssh-client.i686.sh
@@ -2,13 +2,15 @@
 echo 'building dropbear client [i686]'
 
 cd ./../build/dropbear
+make clean
 ./configure --disable-zlib
 LTM_CFLAGS=-Os LDFLAGS="-Wl --gc-sections --static" CROSS_COMPILE=i686-linux-musl-cross/bin/i686-linux-musl- CFLAGS="-ffunction-sections -fdata-sections" ARCH=ix86_32
 #	Optimizations as per Recommendation: https://github.com/mkj/dropbear/blob/master/SMALL.md
 #	Cross-Compilation as per: http://landley.net/toybox/index.html
-make PROGRAMS="dbclient" MULTI=1
+make PROGRAMS="dbclient" MULTI=1 ARCH=ix86_32
 #	Making a single binary as per: https://github.com/mkj/dropbear/blob/master/MULTI.md
 mv ./dropbearmulti ./../1440k-fdd
+
 cd ./../../scripts
 
 exit
