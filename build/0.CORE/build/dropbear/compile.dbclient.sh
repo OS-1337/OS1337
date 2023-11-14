bf95c334fbe2cd95c394a08f10ae5eededecbb00
Kevin Karhan <kevin@karhan.guru>
2023-11-14 18:55:59 +0100

    - adding first [unsuccessful!!!] build files - .config files -- linux-6.6.1-i486-minimal.config -- toybox-0.8.10-i486-minimal.config -- dbclient-2022.83

--- build/0.CORE/build/dropbear/compile.dbclient.sh
+++ build/0.CORE/build/dropbear/compile.dbclient.sh
@@ -2,4 +2,5 @@
 ./configure --disable-zlib --disable-x11 --disable-agent-forwarding
 LDFLAGS=-W1,--gc-sections,--static CROSS_COMPILE=i486-linux-musl-cross/bin/i486-linux-musl- CFLAGS="-Os -ffunction-sections -fdata-sections" make ARCH=x86 dbclient
 ls -alh
+echo 'done!'
 exit
