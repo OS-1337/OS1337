0721ce9292732984c692124fb02dba5fb7d0f450
Kevin Karhan <kevin@karhan.guru>
2023-12-04 01:59:39 +0100

    Committing fixes for the bootup [init] [as per comment in issue](https://github.com/OS-1337/OS1337/issues/2#issuecomment-1837642809):
    - moved init file to /etc/init aka. [where Linux expects it to be](https://github.com/OS-1337/OS1337/issues/2#issuecomment-1837640692)
    
    - added some basic aliases to /etc/profile [from my personal .bash_aliases](https://github.com/kkarhan/misc-scripts/blob/master/bash/.bash_aliases)
    
    - made some fancy ASCII drawing bars on welcome.txt
    - - see ascii-drawing-chars.txt for those.
    
    - rebuilt rootfs.cpio.xz and os1337.img

--- build/0.CORE/build/image/create.initramfs.sh
+++ build/0.CORE/build/image/create.initramfs.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 echo 'compressing filesystem into xz-compressed cpio initramfs'
 cd ./../../fdd/fs
+chmod +x ./etc/init
+sudo chown -R root:root .
 find . | cpio -H newc -o | xz -9 > ../rootfs.cpio.xz
 ls -alh
 echo 'done!'
