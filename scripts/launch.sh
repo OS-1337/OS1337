567d81fad3a043630b14e0a24dfa2725a2cbb663
SweetAI Belle <SweetAIBelle@gmail.com>
2023-12-18 13:05:23 -0800

    Fix up some permissions, and the launch.sh script.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,8 @@
-#! /usr/bin/env bash
+#!/bin/bash
+
 source ./config.sh
 
+cd ./../build/$base_dir/
+
 # Start up the image in qemu
-qemu-system-i386 -drive file=o$image_name,format=raw,index=0,media=disk
+qemu-system-i386 -drive file=$image_name,format=raw,index=0,media=disk
