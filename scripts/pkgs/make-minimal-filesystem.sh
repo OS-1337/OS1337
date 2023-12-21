10fefce8c74c5ce3867f4c4525e9e6d55fe39bc9
SweetAI Belle <SweetAIBelle@gmail.com>
2023-12-20 18:32:16 -0800

    Copy init files and syslinux.cfg into the profiles, and use those copies instead of fdd.

--- scripts/pkgs/make-minimal-filesystem.sh
+++ scripts/pkgs/make-minimal-filesystem.sh
@@ -8,9 +8,8 @@ echo 'Creating folders.'
 mkdir -pv ../build/{$base_dir/rootfs/{dev,etc/init.d,proc,sys,tmp,bin},downloads,working}
 
 echo 'Copying files.'
-cp -v ../build/0.CORE/fdd/fs/etc/{init,inittab,profile} ../build/$base_dir/rootfs/etc/
-cp -v ./$profile_dir/welcome.txt ../build/$base_dir/rootfs/
-cp -v ../build/0.CORE/fdd/syslinux.cfg ../build/$base_dir/
+cp -vr ./$profile_dir/rootfs ../build/$base_dir/
+cp -v ./$profile_dir/syslinux.cfg ../build/$base_dir/
 
 echo 'Creating special files needed for system.'
 cd ./../build/$base_dir/rootfs
