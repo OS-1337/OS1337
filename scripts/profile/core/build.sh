6f928cfb8151fba0622d7c9014ba76486f69821b
kkarhan <kevin@karhan.guru>
2024-01-25 07:35:39 +0100

    Changed build order in build.sh for the necessary shift to integrate the initramfs.cpio.xz into bzImage [aka. initrd into linux kernel]... - See [issue #10](https://github.com/OS-1337/OS1337/issues/10#issuecomment-1909435941) for details.
    Signed-off-by: kkarhan <kevin@karhan.guru>

--- scripts/profile/core/build.sh
+++ scripts/profile/core/build.sh
@@ -7,9 +7,6 @@ echo "Current directory is $PWD"
 ./pkgs/musl-cross.sh
 #read -p "Press any key to continue... " -n1 -s
 
-./pkgs/build-linux.sh
-#read -p "Press any key to continue... " -n1 -s
-
 ./pkgs/build-toybox.sh
 #read -p "Press any key to continue... " -n1 -s
 
@@ -17,6 +14,9 @@ echo "Current directory is $PWD"
 #read -p "Press any key to continue... " -n1 -s
 
 ./pkgs/make-initramfs.sh
+#read -p "Press any key to continue... " -n1 -s
+
+./pkgs/build-linux.sh
 #read -p "Press any key to continue... " -n1 -s
 
 ./pkgs/make-image.sh
