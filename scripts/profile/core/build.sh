4ee9247283774fbe88ee38931b39967d35ab1c20
Kevin Karhan <kevin@karhan.guru>
2024-08-19 23:36:28 +0200

    Corrected the build order of scripts as necessary: 1. musl-cross 2. userland 3. additional programs 4. initramfs 5. linux kernel 6. disk image 7. filesystem 8. bootloader

--- scripts/profile/core/build.sh
+++ scripts/profile/core/build.sh
@@ -3,27 +3,40 @@
 
 echo "Current directory is $PWD"
 
-./pkgs/make-minimal-filesystem.sh
-#read -p "Press any key to continue... " -n1 -s
-
+#	Download musl-cross, if it isn't present..
 ./pkgs/musl-cross.sh
-#read -p "Press any key to continue... " -n1 -s
+#	read -p "Press any key to continue... " -n1 -s
 
+#	Download toybox, if it isn't present, then build it.
 ./pkgs/build-toybox.sh
-#read -p "Press any key to continue... " -n1 -s
+#	read -p "Press any key to continue... " -n1 -s
 
+#	Download dropbear, if it isn't present, then build dbclient.
 ./pkgs/build-dbclient.sh
-#read -p "Press any key to continue... " -n1 -s
+#	read -p "Press any key to continue... " -n1 -s
 
-./pkgs/build-mlb.sh
-#read -p "Press any key to continue... " -n1 -s
-
+#	Create the initramfs structure.
 ./pkgs/make-initramfs.sh
-#read -p "Press any key to continue... " -n1 -s
+#	read -p "Press any key to continue... " -n1 -s
 
+#	Download and Build the linux kernel.
+#	-	since the initramfs is being included into it [for ease of use with mlb], it needs to be build *after* the initramfs.
 ./pkgs/build-linux.sh
-#read -p "Press any key to continue... " -n1 -s
+#	read -p "Press any key to continue... " -n1 -s
 
+#	Generate the image file for booting.
 ./pkgs/make-image.sh
+#	read -p "Press any key to continue... " -n1 -s
+
+#	Format the image with a clean filesystem in.
+./pkgs/make-minimal-filesystem.sh
+#	read -p "Press any key to continue... " -n1 -s
+
+#	Download and build mlb, if it doesn't exist.
+#	-	Then install mlb as bootloader onto the image.
+./pkgs/build-mlb.sh
+#	read -p "Press any key to continue... " -n1 -s
+
+echo "OS/1337 was build. You'll find it under $PWD/os1337.img."
 
 exit
